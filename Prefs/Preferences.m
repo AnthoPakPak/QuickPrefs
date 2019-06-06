6428315a5aa8ddd5075881c868d1029857fcf7ec
Cocagne Anthonin <anthococagne@hotmail.fr>
2019-06-06 19:33:42 +0200

    Clean preferences UITextField workaround attempt for Cephei not saving an active textfield (was kept for reference)

--- Prefs/Preferences.m
+++ Prefs/Preferences.m
@@ -18,15 +18,6 @@
 -(void) viewDidLoad {
     [super viewDidLoad];
 
-    // NSArray<UITextField*>* allTextFields = [self findAllTextFieldsInView:[self view]];
-    // DLog(@"allTextFields %@", allTextFields);
-    // for (UITextField *textField in allTextFields) {
-    //     if (textField.isFirstResponder) {
-    //         DLog(@"isFirstResponder");
-    //         textField.delegate = self;
-    //     }
-    // }
-
     self.table.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
 }
 
@@ -51,59 +42,4 @@
     [HBRespringController respring];
 }
 
-
-#pragma mark - Cephei not saving textfield content workaround attempt (just kept for the record)
-
-// Boolean savePreferencesDictionary(CFStringRef appID, CFDictionaryRef dict) {
-// 	CFPreferencesSetMultiple(dict, nil, appID, kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
-// 	return CFPreferencesSynchronize(appID, kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
-// }
-
-// - (void)textFieldDidEndEditing:(UITextField *)textField {
-//     DLog(@"textFieldDidEndEditing");
-
-//     [textField resignFirstResponder];
-// }
-
-// - (void)applySettings:(id)sender {
-//     DLog(@"applySettings");
-
-//     // [self.view endEditing:YES];
-//     [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
-//     NSArray<UITextField*>* allTextFields = [self findAllTextFieldsInView:[self view]];
-//     DLog(@"allTextFields %@", allTextFields);
-    
-
-
-
-//     NSMutableDictionary *settings = [[NSMutableDictionary alloc] initWithContentsOfFile:PLIST_FILE];
-
-//     for (int i = 0; i < 4; i++) {
-//         UITextField *textField = allTextFields[i];
-//         [settings setObject:textField.text forKey:[NSString stringWithFormat:@"item%d", i+1]];
-//     }
-//     [settings writeToFile:PLIST_FILE atomically:NO];
-
-
-//     BOOL b = (BOOL)savePreferencesDictionary((CFStringRef)@"com.anthopak.quickprefs", (CFDictionaryRef)settings);
-
-//     DLog(@"saved ? %d", b);
-//     DLog(@"settings ? %@", settings);
-
-//     CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (CFStringRef)@"com.anthopak.quickprefs/ReloadPrefs", NULL, (CFDictionaryRef)settings, true);
-// }
-
-// -(NSArray*)findAllTextFieldsInView:(UIView*)view{
-//     NSMutableArray* textfieldarray = [[[NSMutableArray alloc] init] autorelease];
-//     for(id x in [view subviews]){
-//         if([x isKindOfClass:[UITextField class]])
-//             [textfieldarray addObject:x];
-
-//         if([x respondsToSelector:@selector(subviews)]){
-//             // if it has subviews, loop through those, too
-//             [textfieldarray addObjectsFromArray:[self findAllTextFieldsInView:x]];
-//         }
-//     }
-//     return textfieldarray;
-// }
 @end
